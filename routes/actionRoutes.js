1ef1ec40a32bef58f846ae7090559b7082b3e653
namistarr <namistarr@icloud.com>
2019-09-06 11:16:40 -0700

    fleshing out actions

--- routes/actionRoutes.js
+++ routes/actionRoutes.js
@@ -5,29 +5,27 @@ const router = express.Router();
 
 router.use(express.json());
 
-//GET all actions
-router.get('/', (req, res) => {
-    actionDb.get() 
-        .then(actions => {
-            res.status(200).json(actions);
-        })
-        .catch(error => {
-            res.status(500).json(error);
-        })
-});
-
 //GET action by id
 router.get('/:id', (req, res) => {
     const id = req.params.id;
-
-    actionDb.get(id) 
-        .then(results => {
-            res.status(200).json(results);
+    actionDb.get(id)
+        .then(action => {
+            if (action) {
+               res.status(200).json(action) 
+            } 
+            else {
+                res.status(404).json({
+                    error: 'There is no action with that ID.'
+                })
+            }
         })
-        .catch(error => {
-            res.status(500).json(error);
+        .catch(() => {
+            res.status(500).json({
+                error: 'There was an error retrieving the action.'
+            })
         })
 });
+
 
 //POST action
 router.post('/', (req, res) => {
@@ -38,7 +36,9 @@ router.post('/', (req, res) => {
             res.status(200).json(newAction)
         })
         .catch(error => {
-            res.status(500).json(error)
+            res.status(500).json({
+                error: 'There was an error adding the action.'
+            })
         })
 })
 
